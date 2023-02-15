2cc0af89839661423ed06a08f7f1be9e9b068846
Kiril Tucker <kiril.tucker@gmail.com>
2023-02-15 22:58:58 +0100

    Improved Loading Animation

--- Experience/Utils/Resources.js
+++ Experience/Utils/Resources.js
@@ -15,16 +15,30 @@ export default class Resources extends EventEmitter {
     this.items = {}
     this.queue = this.assets.length
     this.loaded = 0
+    this.loadingProgress = document.querySelector('.loading-progress')
 
+    this.setLoadingManager()
     this.setLoaders()
     this.startLoading()
   }
 
+  setLoadingManager() {
+    this.loadingManager = new THREE.LoadingManager(
+      // Loaded
+      () => {},
+      // Progress
+      (itemUrl, itemsLoaded, itemsTotal) => {
+        this.progressRatio = (itemsLoaded / itemsTotal) * 100
+        this.loadingProgress.innerHTML = Math.round(this.progressRatio)
+      }
+    )
+  }
+
   setLoaders() {
     this.loaders = {}
-    this.loaders.textureLoader = new THREE.TextureLoader()
-    this.loaders.gltfLoader = new GLTFLoader()
-    this.loaders.dracoLoader = new DRACOLoader()
+    this.loaders.textureLoader = new THREE.TextureLoader(this.loadingManager)
+    this.loaders.gltfLoader = new GLTFLoader(this.loadingManager)
+    this.loaders.dracoLoader = new DRACOLoader(this.loadingManager)
     this.loaders.dracoLoader.setDecoderPath("/draco/")
     this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader)
   }
